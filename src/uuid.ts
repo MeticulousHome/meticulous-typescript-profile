073743de5d4e91a898feca54c9c4c7a08bd75f57
Mimoja <mimoja@meticuloushome.com>
2024-04-08 16:00:43 +0200

    fix(uuid): Remove uuid validation for now as it is refusing valid UUIDs
    Signed-off-by: Mimoja <mimoja@meticuloushome.com>

--- src/uuid.ts
+++ src/uuid.ts
@@ -4,14 +4,7 @@ export class UUID {
   private value: string;
 
   constructor(uuidString: string) {
-    if (!UUID.isValidUUID(uuidString)) {
-      throw new Error('Invalid UUID format.');
-    }
     this.value = uuidString;
-  }
-
-  static isValidUUID(uuid: string): boolean {
-    return validate(uuid);
   }
 
   toString(): string {
