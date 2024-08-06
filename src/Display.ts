65b0f993bdd8dada034256581a96e720dca2f1fd
Mimoja <mimoja@mimoja.de>
2024-08-06 14:45:17 +0200

    feat(display): add descriptions according to schema
    Signed-off-by: Mimoja <mimoja@mimoja.de>

--- src/Display.ts
+++ src/Display.ts
@@ -1,4 +1,8 @@
+// As defined in the JSON schema
+export const SHORT_DESCRIPTION_MAX_LENGTH = 100;
 export interface Display {
   image?: string;
   accentColor?: string;
+  shortDescription?: string;
+  description?: string;
 }
